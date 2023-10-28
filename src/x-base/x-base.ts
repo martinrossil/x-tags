44db28b956a7bb43007b0aac943ef2e9bd45ab7c
Martin Rossil <martinrossil@gmail.com>
2023-10-28 23:26:44 +0200

    feat: update x-base

--- src/x-base/x-base.ts
+++ src/x-base/x-base.ts
@@ -5,6 +5,11 @@
 export default class XBase extends HTMLElement {
 	public constructor() {
 		super();
+		console.log('XBase');
+		this.style.display = 'inline-block';
+		this.style.width = '200px';
+		this.style.height = '200px';
+		this.style.background = 'red';
 	}
 }
 customElements.define('x-base', XBase);
