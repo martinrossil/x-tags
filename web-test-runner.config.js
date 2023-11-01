d7a4ab7d3cc9a8ba637bfa2e53171caa6c4c83fc
Martin Rossil <martinrossil@gmail.com>
2023-11-01 08:50:03 +0100

    test: add summary reporting

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,5 +1,13 @@
 import { esbuildPlugin } from '@web/dev-server-esbuild';
+import { defaultReporter, summaryReporter } from '@web/test-runner';
 
 export default {
-    plugins: [esbuildPlugin({ ts: true })]
+    plugins: [esbuildPlugin({ ts: true })],
+	reporters: [
+        summaryReporter(),
+        defaultReporter({
+            reportTestResults: true,
+            reportTestProgress: true,
+        }),
+    ],
 };
