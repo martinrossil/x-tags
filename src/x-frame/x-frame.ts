c0ce41a38a165f8863c96b7429a2150082f3a4ef
Martin Rossil <martinrossil@gmail.com>
2023-11-01 08:52:05 +0100

    feat: create x-frame closes #19

---
+++ src/x-frame/x-frame.ts
@@ -0,0 +1,14 @@
+import XBase from '../x-base/x-base';
+
+export default class XFrame extends XBase {
+	public constructor() {
+		super();
+		console.log('x-frame construct()');
+	}
+
+	public connectedCallback() {
+		console.log('x-frame connectedCallback');
+		console.log('x-frame child count', this.childElementCount);
+	}
+}
+customElements.define('x-frame', XFrame);
