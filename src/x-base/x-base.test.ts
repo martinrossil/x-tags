bd9e0bababc178341dc4ee63f70aa9304d08d54a
Martin Rossil <martinrossil@gmail.com>
2023-11-01 09:13:09 +0100

    test: wrap x-base inside x-app

--- src/x-base/x-base.test.ts
+++ src/x-base/x-base.test.ts
@@ -8,17 +8,19 @@ XBase;
 describe('x-base', () => {
 	it('Description here', async () => {
 		document.body.innerHTML = `
-		<x-base id="outer" name="outer">
-			<x-fill>
-				<x-color>#123456</x-color>
-			</x-fill>
-			<x-base name="inner">
-				<x-base name="deepest">
-					<x-base name="even deeper">Hello from even deeper 3</x-base>
-					<x-base name="even deeper"></x-base>
+		<x-app>
+			<x-base id="outer" name="outer">
+				<x-fill>
+					<x-color>#123456</x-color>
+				</x-fill>
+				<x-base name="inner">
+					<x-base name="deepest">
+						<x-base name="even deeper">Hello from even deeper 3</x-base>
+						<x-base name="even deeper"></x-base>
+					</x-base>
 				</x-base>
 			</x-base>
-		</x-base>`;
+		</x-app>`;
 
 		const element = document.getElementById('outer');
 		if (element) {
