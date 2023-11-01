d58314642fd75f2dec4e269e51c6eb37216aa241
Martin Rossil <martinrossil@gmail.com>
2023-11-01 22:18:35 +0100

    refactor: out commented logs

--- src/x-frame/x-frame.ts
+++ src/x-frame/x-frame.ts
@@ -3,12 +3,12 @@ import XBase from '../x-base/x-base';
 export default class XFrame extends XBase {
 	public constructor() {
 		super();
-		console.log('x-frame construct()');
+		// console.log('x-frame construct()');
 	}
 
 	public connectedCallback() {
-		console.log('x-frame connectedCallback');
-		console.log('x-frame child count', this.childElementCount);
+		// console.log('x-frame connectedCallback');
+		// console.log('x-frame child count', this.childElementCount);
 	}
 }
 customElements.define('x-frame', XFrame);
