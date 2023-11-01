5d855e40dade40a00cd9ff4421ff72d7975ef28b
Martin Rossil <martinrossil@gmail.com>
2023-11-01 22:34:13 +0100

    test: add x-frame to test suite

--- src/x-frame/x-frame.test.ts
+++ src/x-frame/x-frame.test.ts
@@ -2,10 +2,19 @@ import XFrame from './x-frame';
 
 XFrame;
 
+afterEach(() => {
+	document.body.innerHTML = '';
+});
+
 describe('x-frame', () => {
 	describe('padding attribute', () => {
 		it('', () => {
-			//
+			document.body.innerHTML = `
+				<x-app>
+					<x-frame padding="24" id="element"></x-frame>
+				</x-app>`;
+			const element = document.getElementById('element');
+			// console.log(element);
 		});
 	});
 });
