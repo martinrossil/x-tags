ff346c8da314e5e7005c1f573d5f8bde14b4254d
Martin Rossil <martinrossil@gmail.com>
2023-11-01 08:55:52 +0100

    refactor: x-base.ts

--- src/x-base/x-base.ts
+++ src/x-base/x-base.ts
@@ -5,14 +5,12 @@
 export default class XBase extends HTMLElement {
 	public constructor() {
 		super();
-		console.log('XBase construct', 'atribute [' + this.getAttribute('name') + ']');
-		this.style.outline = 'solid blue 10px';
-		this.style.outlineColor = 'linear-gradient(103deg, #AA24BF 14.31%, rgba(170, 36, 191, 0.00) 83.65%)';
+		console.log('x-base construct()');
 	}
 
 	public connectedCallback() {
-		console.log('connectedCallback ', 'atribute [' + this.getAttribute('name') + ']');
-		console.log('child count', this.childElementCount);
+		console.log('x-base connectedCallback');
+		console.log('x-base child count', this.childElementCount);
 	}
 }
 customElements.define('x-base', XBase);
