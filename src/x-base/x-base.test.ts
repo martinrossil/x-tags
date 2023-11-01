a6dce98eb12182b9b3722670fba397f5f8fd15c1
Martin Rossil <martinrossil@gmail.com>
2023-11-01 22:33:32 +0100

    test: add test attribute

--- src/x-base/x-base.test.ts
+++ src/x-base/x-base.test.ts
@@ -5,6 +5,10 @@ import { assert } from 'chai';
 
 XBase;
 
+afterEach(() => {
+	document.body.innerHTML = '';
+});
+
 describe('x-base', () => {
 	describe('x-base', () => {
 		it('width attribute', () => {
@@ -19,6 +23,20 @@ describe('x-base', () => {
 			} else {
 				throw new Error('element is not defined');
 			}
+		});
+
+		it('test attribute', () => {
+			document.body.innerHTML = `
+				<x-app>
+					<x-base id="element" test="test"></x-base>
+				</x-app>`;
+			/* const element = document.getElementById('element');
+			if (element) {
+				const bound = element.getBoundingClientRect();
+				assert.strictEqual(bound.width, 200);
+			} else {
+				throw new Error('element is not defined');
+			} */
 		});
 
 		it('height attribute', () => {
