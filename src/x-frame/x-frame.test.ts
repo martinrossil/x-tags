b48b41d5e7858db9b200b89e4a7361766c6b82cc
Martin Rossil <martinrossil@gmail.com>
2023-11-02 14:29:23 +0100

    test(padding): create padding tests

--- src/x-frame/x-frame.test.ts
+++ src/x-frame/x-frame.test.ts
@@ -1,3 +1,4 @@
+import { assert } from 'chai';
 import XFrame from './x-frame';
 
 XFrame;
@@ -8,13 +9,28 @@ afterEach(() => {
 
 describe('x-frame', () => {
 	describe('padding attribute', () => {
-		it('', () => {
-			document.body.innerHTML = `
+		it('given padding is 100, size should be 200x200', () => {
+			document.body.innerHTML = /* html */`
 				<x-app>
-					<x-frame padding="24" id="element"></x-frame>
+					<x-frame padding="100" id="element"></x-frame>
 				</x-app>`;
 			const element = document.getElementById('element');
-			// console.log(element);
+			if (element) {
+				const bound = element.getBoundingClientRect();
+				assert.strictEqual(bound.width, 200);
+				assert.strictEqual(bound.height, 200);
+			} else {
+				throw new Error('element is not defined');
+			}
+		});
+
+		it('given padding is 50 and a child size 200x200, size should be 300x300', () => {
+			document.body.innerHTML = /* html */`
+			<x-app>
+				<x-frame padding="50" id="element">
+					<x-base width="200" height="200" width></x-base>
+				</x-frame>
+			</x-app>`;
 		});
 	});
 });
