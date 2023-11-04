769e4c56097c0175539fe813742b9dbc15265154
Martin Rossil <martinrossil@gmail.com>
2023-11-04 15:37:16 +0100

    test(padding): add padding test

--- src/x-frame/x-frame.test.ts
+++ src/x-frame/x-frame.test.ts
@@ -1,6 +1,8 @@
 import { assert } from 'chai';
 import XFrame from './x-frame';
+import XApp from '../x-app/x-app';
 
+XApp;
 XFrame;
 
 afterEach(() => {
@@ -12,9 +14,9 @@ describe('x-frame', () => {
 		it('given padding is 100, size should be 200x200', () => {
 			document.body.innerHTML = /* html */`
 				<x-app>
-					<x-frame padding="100" id="element"></x-frame>
+					<x-frame id="x-frame" padding="100"></x-frame>
 				</x-app>`;
-			const element = document.getElementById('element');
+			const element = document.getElementById('x-frame');
 			if (element) {
 				const bound = element.getBoundingClientRect();
 				assert.strictEqual(bound.width, 200);
@@ -27,17 +29,25 @@ describe('x-frame', () => {
 		it('given padding is 50 and a child size 200x200, size should be 300x300', () => {
 			document.body.innerHTML = /* html */`
 			<x-app>
-				<x-frame padding="50" id="element">
-					<x-base width="200" height="200" width></x-base>
+				<x-frame id="x-frame" padding="50">
+					<x-base width="200" height="200"></x-base>
 				</x-frame>
 			</x-app>`;
+			const element = document.getElementById('x-frame');
+			if (element) {
+				const bound = element.getBoundingClientRect();
+				assert.strictEqual(bound.width, 300);
+				assert.strictEqual(bound.height, 300);
+			} else {
+				throw new Error('element is not defined');
+			}
 		});
 
 		it('test attribute', () => {
 			document.body.innerHTML = /* html */`
-				<x-app>
-					<x-frame test="test"></x-frame>
-				</x-app>`;
+			<x-app>
+				<x-frame test="test"></x-frame>
+			</x-app>`;
 		});
 	});
 });
