6b59fac282b884848210bd44923ea145e3d8c089
Martin Rossil <martinrossil@gmail.com>
2023-11-04 15:34:10 +0100

    tes(x-base)t: add default styles

--- src/x-base/x-base.test.ts
+++ src/x-base/x-base.test.ts
@@ -1,6 +1,8 @@
+import XApp from '../x-app/x-app';
 import XBase from './x-base';
 import { assert } from 'chai';
 
+XApp;
 XBase;
 
 afterEach(() => {
@@ -8,14 +10,58 @@ afterEach(() => {
 });
 
 describe('x-base', () => {
-	describe('x-base', () => {
+	describe('default styles', () => {
+		it('display should be inline-block', () => {
+			document.body.innerHTML = /* html */`
+			<x-app>
+				<x-base id="x-base"></x-base>
+			</x-app>`;
+
+			const element = document.getElementById('x-base');
+			if (element) {
+				assert.strictEqual(element.style.display, 'inline-block');
+			} else {
+				throw new Error('element is not defined');
+			}
+		});
+
+		it('lineHeight should be 0', () => {
+			document.body.innerHTML = /* html */`
+			<x-app>
+				<x-base id="x-base"></x-base>
+			</x-app>`;
+
+			const element = document.getElementById('x-base');
+			if (element) {
+				assert.strictEqual(element.style.lineHeight, '0px');
+			} else {
+				throw new Error('element is not defined');
+			}
+		});
+
+		it('fontSize should be 0', () => {
+			document.body.innerHTML = /* html */`
+			<x-app>
+				<x-base id="x-base"></x-base>
+			</x-app>`;
+
+			const element = document.getElementById('x-base');
+			if (element) {
+				assert.strictEqual(element.style.fontSize, '0px');
+			} else {
+				throw new Error('element is not defined');
+			}
+		});
+	});
+
+	describe('attributes', () => {
 		it('width attribute', () => {
 			document.body.innerHTML = /* html */`
-				<x-app>
-					<x-base id="element" width="200" height="200"></x-base>
-				</x-app>`;
+			<x-app>
+				<x-base id="x-base" width="200" height="200"></x-base>
+			</x-app>`;
 
-			const element = document.getElementById('element');
+			const element = document.getElementById('x-base');
 			if (element) {
 				const bound = element.getBoundingClientRect();
 				assert.strictEqual(bound.width, 200);
@@ -26,17 +72,17 @@ describe('x-base', () => {
 
 		it('test attribute', () => {
 			document.body.innerHTML = /* html */`
-				<x-app>
-					<x-base test="test"></x-base>
-				</x-app>`;
+			<x-app>
+				<x-base test="test"></x-base>
+			</x-app>`;
 		});
 
 		it('height attribute', () => {
 			document.body.innerHTML = /* html */`
-				<x-app>
-					<x-base id="element" width="200" height="200"></x-base>
-				</x-app>`;
-			const element = document.getElementById('element');
+			<x-app>
+				<x-base id="x-base" width="200" height="200"></x-base>
+			</x-app>`;
+			const element = document.getElementById('x-base');
 			if (element) {
 				const bound = element.getBoundingClientRect();
 				assert.strictEqual(bound.height, 200);
