ad39b4e15e2b4189d0f12bca460f52bc527ffff6
Martin Rossil <martinrossil@gmail.com>
2023-11-04 15:38:43 +0100

    feat(x-hex): create hex.ts

---
+++ src/x-hex/x-hex.ts
@@ -0,0 +1,47 @@
+import XProperty from '../x-property/x-property';
+
+export default class XHex extends XProperty {
+	public static get observedAttributes() {
+		return ['hex', 'alpha'];
+	}
+
+	private _value = '';
+
+	private _alpha = '';
+
+	private connected = false;
+
+	public constructor() {
+		super();
+		// console.log('x-hex construct()');
+	}
+
+	public attributeChangedCallback(name: 'hex' | 'alpha', oldValue: string, newValue: string) {
+		// console.log('x-hex attribute changed', name, newValue);
+		switch (name) {
+			case 'hex': this.hexAttributeChanged(newValue);
+				break;
+			case 'alpha': this.alphaAttributeChanged(newValue);
+				break;
+			default: break;
+		}
+	}
+
+	public connectedCallback() {
+		// console.log('h-hex connectedCallback()');
+		this.connected = true;
+	}
+
+	// check for connected === true, notify parents if true
+	private hexAttributeChanged(value: string) {
+		// console.log('value', value, this.connected);
+		this._value = value;
+	}
+
+	// check for connected === true, notify parents if true
+	private alphaAttributeChanged(value: string) {
+		// console.log('alpha', value, this.connected);
+		this._alpha = value;
+	}
+}
+customElements.define('x-hex', XHex);
