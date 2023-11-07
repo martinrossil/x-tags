d5666b6678c3e80cffe3cc7d3705029bc1632411
Martin Rossil <martinrossil@gmail.com>
2023-11-07 23:07:15 +0100

    feat(x-horizontal): create x-horizontal.ts

--- src/x-horizontal/x-horizontal.ts
+++ src/x-horizontal/x-horizontal.ts
@@ -1,9 +1,18 @@
 import XProperty from '../x-property/x-property';
 
 export default class XHorizontal extends XProperty {
+	public static get observedAttributes() {
+		return ['left', 'right', 'center'];
+	}
+
 	public constructor() {
 		super();
-		console.log('XHorizontal');
+		// console.log('XHorizontal');
+		// left: calc(50% - 100px + 100px);
+	}
+
+	public attributeChangedCallback(name: 'left' | 'right' | 'center', oldValue: string, newValue: string) {
+		// console.log('x-horizontal attribute', name, oldValue, '[' + newValue + ']');
 	}
 }
 customElements.define('x-horizontal', XHorizontal);
