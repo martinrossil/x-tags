93b89afe998ecbe5876361bb55ccb4273888c079
Martin Rossil <martinrossil@gmail.com>
2023-11-07 23:05:00 +0100

    refactor(x-vertical): remove scale

--- src/x-vertical/x-vertical.ts
+++ src/x-vertical/x-vertical.ts
@@ -2,15 +2,16 @@ import XProperty from '../x-property/x-property';
 
 export default class XVertical extends XProperty {
 	public static get observedAttributes() {
-		return ['top', 'bottom', 'center', 'scale'];
+		return ['top', 'bottom', 'center'];
 	}
 
 	public constructor() {
 		super();
 		console.log('XVertical');
+		// left: calc(50% - 100px + 100px);
 	}
 
-	public attributeChangedCallback(name: 'top' | 'bottom' | 'center' | 'scale', oldValue: string, newValue: string) {
+	public attributeChangedCallback(name: 'top' | 'bottom' | 'center', oldValue: string, newValue: string) {
 		console.log('x-vertical attribute', name, oldValue, '[' + newValue + ']');
 	}
 }
