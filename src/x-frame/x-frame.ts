04af2893fc96b3cb40c876d2ce0bcec7e1ea5c31
Martin Rossil <martinrossil@gmail.com>
2023-11-07 23:08:28 +0100

    feat(layout): implement absolute layout

--- src/x-frame/x-frame.ts
+++ src/x-frame/x-frame.ts
@@ -1,3 +1,4 @@
+import { getChildTag } from '../utils/tags';
 import XElement from '../x-element/x-element';
 
 export default class XFrame extends XElement {
@@ -8,6 +9,8 @@ export default class XFrame extends XElement {
 	public constructor() {
 		super();
 		// console.log('x-frame construct()');
+		// console.log(this.children);
+		// this.setLayout();
 	}
 
 	public attributeChangedCallback(name: string, oldValue: string, newValue: string) {
@@ -17,6 +20,90 @@ export default class XFrame extends XElement {
 			case 'padding': this.paddingAttributeChanged(parseInt(newValue, 10));
 				break;
 			default: break;
+		}
+	}
+
+	protected connectedCallback() {
+		super.connectedCallback();
+		// console.log('x-frame connectedCallback()');
+		this.setLayout();
+	}
+
+	private setLayout() {
+		// console.log('setLayout()');
+		const autoLayout = getChildTag(this, 'X-AUTO-LAYOUT');
+		if (autoLayout) {
+			this.setAutoLayout();
+		} else {
+			this.setAbsoluteLayout();
+		}
+	}
+
+	private setAutoLayout() {
+		// console.log('setAutoLayout()');
+	}
+
+	private setAbsoluteLayout() {
+		// console.log('setAbsoluteLayout()');
+		this.style.position = 'relative';
+		let html: HTMLElement;
+		for (const element of this.children) {
+			html = element as HTMLElement;
+			if (html.tagName === 'X-FRAME' || html.tagName === 'X-ELEMENT') {
+				// console.log('found tagName', html.tagName);
+				html.style.position = 'absolute';
+				// console.log(html);
+				const constraints = getChildTag(html, 'X-CONSTRAINTS');
+				if (constraints) {
+					// console.log('found constraints', constraints);
+					this.setChildConstraintsStyles(html, constraints);
+				}
+			}
+		}
+	}
+
+	private setChildConstraintsStyles(element: HTMLElement, constraints: Element) {
+		// const width = element.getAttribute('width');
+		// console.log('width [' + width + ']');
+		// const height = element.getAttribute('height');
+		// console.log('height [' + height + ']');
+		const horizontal = getChildTag(constraints, 'X-HORIZONTAL');
+		if (horizontal) {
+			const left = horizontal.getAttribute('left');
+			const right = horizontal.getAttribute('right');
+			const center = horizontal.getAttribute('center');
+
+			if (left !== null) {
+				const leftNumber = parseFloat(left);
+				if (!isNaN(leftNumber)) {
+					element.style.left = leftNumber + 'px';
+				}
+			}
+
+			if (right !== null) {
+				const rightNumber = parseFloat(right);
+				if (!isNaN(rightNumber)) {
+					element.style.right = rightNumber + 'px';
+				}
+			}
+
+			if (center !== null) {
+				const width = element.getAttribute('width');
+				let widthNumber = NaN;
+				if (width !== null) {
+					widthNumber = parseFloat(width);
+					if (isNaN(widthNumber)) {
+						widthNumber = 0;
+					}
+				}
+
+				let centerNumber = parseFloat(center);
+				if (isNaN(centerNumber)) {
+					centerNumber = 0;
+				}
+
+				element.style.left = 'calc(50% + ' + (-(widthNumber * 0.5) + centerNumber) + 'px)';
+			}
 		}
 	}
 
